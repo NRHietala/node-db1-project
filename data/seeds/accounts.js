899cd030ed72d7546e6e726e2fa2c9a2e557c468
Nathan Hietala <Nrhietala@gmail.com>
2021-02-16 10:03:20 -0600

    fixed spacing issue inside accounts.js

--- data/seeds/accounts.js
+++ data/seeds/accounts.js
@@ -1,4 +1,4 @@
-exports.seed = function (knex, Promise) {
+exports.seed = function(knex, Promise) {
   return knex('accounts').truncate()
     .then(function () {
       return knex('accounts').insert([
